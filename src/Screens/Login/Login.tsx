3480283f80d24e44477a980945583e3363930b5d
Happy1353 <79153940348@yandex.ru>
2023-05-04 16:05:39 +0200

    add login screen

---
+++ src/Screens/Login/Login.tsx
@@ -0,0 +1,126 @@
+import React, {useRef, useState} from 'react';
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+} from 'react-native';
+
+import WELCOMESVG from '../../assets/welcome.svg';
+import EMAIL from '../../assets/email.svg';
+import LOCK from '../../assets/lock.svg';
+
+export const Login = () => {
+  const [login, setLogin] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const loginRef: any = useRef(null);
+  const passwordRef: any = useRef(null);
+
+  const openLoginTextInput = () => {
+    loginRef.current.focus();
+  };
+
+  const openPasswordTextInput = () => {
+    passwordRef.current.focus();
+  };
+
+  return (
+    <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+      <View style={styles.container}>
+        <SafeAreaView>
+          <WELCOMESVG width={'100%'} />
+        </SafeAreaView>
+        <View style={styles.bottomContainer}>
+          <View>
+            <Text style={styles.title}>Welcome</Text>
+            <Pressable style={styles.input} onPress={openLoginTextInput}>
+              <EMAIL />
+              <TextInput
+                ref={loginRef}
+                style={styles.inputContainer}
+                onChangeText={setLogin}
+                value={login}
+                placeholder="email"
+                placeholderTextColor={'#FFFFFF'}
+              />
+            </Pressable>
+            <Pressable style={styles.input} onPress={openPasswordTextInput}>
+              <LOCK />
+              <TextInput
+                ref={passwordRef}
+                style={styles.inputContainer}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="password"
+                placeholderTextColor={'#FFFFFF'}
+              />
+            </Pressable>
+          </View>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.title}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#9933FF',
+    justifyContent: 'space-between',
+  },
+  bottomContainer: {
+    backgroundColor: '#D9D9D9',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 24,
+  },
+  title: {
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 20,
+    lineHeight: 26,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 21,
+  },
+  input: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#334B6A',
+    borderRadius: 8,
+    height: 52,
+    paddingHorizontal: 12,
+    marginTop: 20,
+  },
+  inputContainer: {
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#FFFFFF',
+    paddingLeft: 8,
+  },
+  button: {
+    justifyContent: 'center',
+    backgroundColor: '#9933FF',
+    borderRadius: 10,
+    height: 52,
+    marginTop: 20,
+  },
+});
